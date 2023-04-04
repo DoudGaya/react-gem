3567e52110bbdeb9f5835f50f795ddb4cdbbf299
DoudGaya <arbdoule@yahoo.com>
2023-04-04 12:45:40 +0100

    pages

--- src/pages/home/Banner.tsx
+++ src/pages/home/Banner.tsx
@@ -1,15 +1,13 @@
 import banner from '../../assets/banner.jpg'
+import { Search } from './Seacrch'
 
 export const Banner = () => {
-    return ( 
+    return (
         <div className=" h-[70vh] bg-center bg-fixed bg-cover bg-no-repeat flex bg-blue-400 justify-center flex-col items-center" style={{backgroundImage: `url(${banner})`}}>
-        <div className=" bg-cyan-950/30 w-full h-full flex flex-col items-center ">
-            <div className="">hello davel</div>
-            <div className=" flex flex-col items-center justify-center h-full space-y-6 w-3/4 ">
-                <h1 className=' text-4xl font-banner text-white'>Welcome to <span className=' text-5xl font-bold'>ManyChef</span> recipes</h1>  
-                <input type="search" className=" w-1/2 py-3 px-4 outline-none rounded-full mx-auto" /> 
+            <div className=" bg-cyan-950/30 w-full h-full flex flex-col items-center ">
+                <div className="">hello davel</div>
+                <Search />
             </div>
-        </div>
         </div>
     )
 }
---
+++ src/pages/home/Seacrch.tsx
@@ -0,0 +1,8 @@
+export const Search = () => {
+    return (
+    <div className=" flex flex-col items-center justify-center h-full space-y-6 w-3/4 ">
+        <h1 className=' text-4xl font-banner text-white'>Welcome to <span className=' text-5xl font-bold'>ManyChef</span> recipes</h1>  
+        <input type="search" className=" w-1/2 py-3 px-4 outline-none rounded-full mx-auto" /> 
+    </div>
+    )
+}
