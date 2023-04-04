d236709d2d83a84df1d037912deb1b78a881255b
DoudGaya <arbdoule@yahoo.com>
2023-04-04 22:28:07 +0100

    mackups

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,24 @@
+import foodImg from '../assets/recipe.jpeg'
 import { Banner } from "./home/Banner"
+
+
+
+
+const recipe = [
+    {
+        name: 'Pepperoni Pizza',
+        image: foodImg
+    },
+    
+]
 export const Home = () => {
     return (
         <>
             <Banner />
-            <div className="">
-
+            <div className=" w-full">
+                <div className="w-[1000px] grid grid-cols-4 -mt-20 mx-auto bg-purple-200">
+                    <div className=""></div>
+                </div>
             </div>
         </>
     )
